26f97cd97c33a698f56afd4f6c5472edb198014b
zeeshan__kharadi <162435122+zeeshan91kharadi@users.noreply.github.com>
2024-12-07 22:22:49 +0530

    Update main.js
    adding e mail contact code

--- main.js
+++ main.js
@@ -5,3 +5,28 @@ var typed = new Typed(".text",{
     backDelay : 1000,
     loop : true
 });
+
+
+const form = document.querySelector("form");
+const fullname = document.getElementById("name");
+const email = document.getElementById("email");
+const subject = document.getElementById("subject");
+const message = document.getElementById("message");
+function sendEmail() {
+  const bodyMessage = `fullname : ${fullname.value}<br> Email: ${email.value} <br> message: ${message.value} `;
+  Email.send({
+    Host: "smtp.elasticemail.com",
+    Username: "jishankharadi42@gmail.com",
+    Password: "3CE04171163465816BEB470D0F8C5466C5BD",
+    To: "jishankharadi42@gmail.com",
+    From: "jishankharadi42@gmail.com",
+    Subject: subject.value,
+    Body: bodyMessage
+  }).then(
+    message => alert(message)
+);
+}
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  sendEmail();
+})
